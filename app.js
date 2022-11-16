5b531195ce34450d0c1061f1a485c6f5da0cc0e6
EugeBr <eugebordenave@gmail.com>
2022-11-16 23:04:56 +0100

    2 steps event listeners

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form"),
     nextBtn = form.querySelector(".nextBtn"),
     backBtn = form.querySelector(".backBtn"),
+    allInput = form.querySelectorAll(".first input");
     submitButton = form.querySelector("#submit")
 
 function cargarOpciones() {
@@ -38,3 +39,15 @@ function submitForm() {
 }
 
 submitButton.addEventListener('click', () => submitForm());
+
+nextBtn.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
